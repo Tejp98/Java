452f5d9db14c249d28d068b4365df86be3dcd84b
Tej <tejmpatel98@gmail.com>
2020-02-20 10:55:47 +0530

    Program to check whether a string can form palindrome string or not

---
+++ Palindrome.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Palindrome{
+	public static void main(String args[]){
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String input = console.nextLine();
+
+		System.out.println("Palindrome:"+ check_palindrome(input));
+
+	}
+	static boolean check_palindrome(String input){
+		
+			
+			
+			char[] input_array = input.toCharArray();
+			int[] check = new int[input_array.length+100];
+			for(int i=0; i < input_array.length; i++){
+				check[input_array[i]] = check[input_array[i]] +1;
+
+			}
+			if(input.length()%2 ==0){
+			for(int i : check){
+				if(check[i]%2!=0){
+					return false;
+				
+				
+
+			}
+
+		}
+		return true;
+}
+		else{
+			int counter= 0;
+			for(int i=0; i <check.length;i++){
+				if(check[i]%2!=0){
+					counter++;
+				}}
+				if(counter ==1){return true;}
+				else {return false;}
+			}
+
+		
+	}
+}
