9fb5a15b31f6da93282412991d2e7c86aa5ee08a
Tej <tejmpatel98@gmail.com>
2020-06-10 10:04:13 +0530

    question included in the comments of the file

---
+++ Is_Subsequence.java
@@ -0,0 +1,62 @@
+// Question : Is Subsequence
+
+
+// Given a string s and a string t, check if s is subsequence of t.
+
+// A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+// Follow up:
+// If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+// Credits:
+// Special thanks to @pbrother for adding this problem and creating all test cases.
+
+ 
+
+// Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 100
+// 0 <= t.length <= 10^4
+// Both strings consists only of lowercase characters.
+
+//  PLease note that only a function that gets all the values of the questions and returns answer is written not the whole implementation.
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        
+        int index1 =0,index2=0;
+        
+        while(index1<s.length() && index2<t.length()){
+            if(s.charAt(index1) == t.charAt(index2)){
+                
+                index1++;
+            }
+            
+            index2++;
+        }
+        if(index1 == s.length()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
