f666591360d386a7e8b4207536a4f5bbe25bfcef
Tej <tejmpatel98@gmail.com>
2020-07-07 11:22:30 +0530

    question included in the comments of the file

---
+++ Implement_Trie_(Prefix_Tree).java
@@ -0,0 +1,113 @@
+// Question : Implement Trie (Prefix Tree)
+
+// Implement a trie with insert, search, and startsWith methods.
+
+// Example:
+
+// Trie trie = new Trie();
+
+// trie.insert("apple");
+// trie.search("apple");   // returns true
+// trie.search("app");     // returns false
+// trie.startsWith("app"); // returns true
+// trie.insert("app");   
+// trie.search("app");     // returns true
+// Note:
+
+// You may assume that all inputs are consist of lowercase letters a-z.
+// All inputs are guaranteed to be non-empty strings.
+
+//  PLease note that only a function that gets all the values of the questions and returns answer is written not the whole implementation.
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class TrieNode {
+
+    
+    private TrieNode[] links;
+
+    private final int R = 26;
+
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch -'a'] != null;
+    }
+    public TrieNode get(char ch) {
+        return links[ch -'a'];
+    }
+    public void put(char ch, TrieNode node) {
+        links[ch -'a'] = node;
+    }
+    public void setEnd() {
+        isEnd = true;
+    }
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
+     
+
+class Trie {
+    
+    
+     private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+    
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+           char curLetter = word.charAt(i);
+           if (node.containsKey(curLetter)) {
+               node = node.get(curLetter);
+           } else {
+               return null;
+           }
+        }
+        return node;
+    }
+
+    
+    public boolean search(String word) {
+       TrieNode node = searchPrefix(word);
+       return node != null && node.isEnd();
+    }
+    
+   public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
